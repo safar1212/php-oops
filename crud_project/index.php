c701a5e0085521497985e491f7ea243dbc072aaf
Safar Ali <safar.alee99@gmail.com>
2024-05-31 16:56:27 +0500

    create update and delete method

--- crud_project/index.php
+++ crud_project/index.php
@@ -1,4 +1,18 @@
 <?php
 include "database.php";
 $obj = new database();
+
+// $obj->insert('students',['student_name'=>"Alee",'age'=>'25','city'=>'London']);
+// echo "The following data is inserted in : ";
+// print_r($obj->getResult());
+// $obj->update('students',['student_name'=>"Hussain Ali",'age'=>'28','city'=>'London'], 'id="11"');
+// echo "The following data is updated in : ";
+// print_r($obj->getResult());
+// $obj->delete('students', 'id="9"');
+// echo "The following data is updated in : ";
+// print_r($obj->getResult());
+$obj->sql('SELECT * FROM students WHERE student_name LIKE "zafar"');
+echo "SQL result is : ";
+print_r($obj->getResult());
+
 ?>
